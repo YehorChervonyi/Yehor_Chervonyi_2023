e37b714cdb3b786ea1bcd5f18e48831b6f32f3e4
Yehor Chervony <chervony.egor@gmail.com>
2023-03-06 00:43:02 +0200

    Dublicate user input numbers

---
+++ Lesson1/task3/task3.cs
@@ -0,0 +1,10 @@
+﻿List<int> usernumbers = new List<int>();
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Type {i + 1} number: ");
+    usernumbers.Add(int.Parse(Console.ReadLine()));
+}
+foreach (int i in usernumbers)
+{
+    Console.WriteLine($"{i}");
+}
