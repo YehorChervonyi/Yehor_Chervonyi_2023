b0eab28919d534907e1b88d32f3672c86e696993
Yehor Chervony <chervony.egor@gmail.com>
2023-03-06 10:01:55 +0200

    Some actions with strings

---
+++ Lesson1/task4/task4.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Type first string: ");
+string userinput1 = Console.ReadLine();
+Console.Write("Type second string: ");
+string userinput2 = Console.ReadLine();
+if (userinput1.Length > userinput2.Length)
+{
+    Console.WriteLine(userinput2 + userinput1);
+}
+else if (userinput2.Length > userinput1.Length && userinput1.Length != userinput2.Length)
+{
+    string[] userinput2split = userinput2.Split(userinput1[0]);
+    foreach (string element in userinput2split)
+    {
+        Console.Write(element+" ");
+    }
+}
