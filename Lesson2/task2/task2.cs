169d1947b0a492c71517d07e311a664e62d03d54
Yehor Chervony <chervony.egor@gmail.com>
2023-03-12 17:54:07 +0200

    Implemented Lesson2 - task2

---
+++ Lesson2/task2/task2.cs
@@ -0,0 +1,37 @@
+﻿Random rnd = new Random();
+List<Person> people = new List<Person>()
+{
+    new Person ("Tom",rnd.Next(1,90)),
+    new Person ("Sam",rnd.Next(1,90)),
+    new Person ("Bob",rnd.Next(1,90)),
+    new Person ("Ban",rnd.Next(1,90))
+};
+ // Show all Persons
+var showall = from x in people select x;
+foreach (var person in showall)
+{
+    Console.WriteLine($"{person.Name} - {person.Age}");
+}
+Console.Write("Write age to start with: ");
+int startage = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write age to end with: ");
+int endage = Convert.ToInt32(Console.ReadLine());
+
+//Main task
+
+var filter = from x in people where x.Age >= startage && x.Age <= endage select x;
+foreach(var person in filter)
+{
+    Console.WriteLine($"{person.Name} - {person.Age}");
+}
+class Person
+{
+    public string Name { get; }
+    public int Age { get; }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
