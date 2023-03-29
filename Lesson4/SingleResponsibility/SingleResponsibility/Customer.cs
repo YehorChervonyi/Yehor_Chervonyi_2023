6d5a4d968497401ca37d7a47e2e5581055ea7f39
Yehor Chervony <chervony.egor@gmail.com>
2023-03-29 23:52:58 +0300

    Changed some name in methods and some logic

---
+++ Lesson4/SingleResponsibility/SingleResponsibility/Customer.cs
@@ -0,0 +1,44 @@
+﻿namespace SingleResponsibility
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        
+        public Customer(int id, string name, decimal balance)
+        {
+            Id = id; 
+            Name = name; 
+            Balance = balance;
+        }
+
+        public List<Customer> CustomersList = new List<Customer>()
+        {
+            new Customer(1, "Fikus", 0),
+            new Customer(2, "VHarbar", 100000)
+        };
+
+        public void GetBalance(int id)
+        {
+            Console.WriteLine($"Your balance is: {GetBalanceById(id)}");
+        }
+        public decimal GetBalanceById(int id)
+        {
+            return GetCustomerById(id).Balance;
+        }
+        public Customer GetCustomerById(int id)
+        {
+            return CustomersList.FirstOrDefault(x => x.Id == id);
+        }
+        public void SaveToDatabase()
+        {
+            Console.WriteLine("Saved!");
+        }
+        public void UpdateBalance(int id, decimal newBalance)
+        {
+            GetCustomerById(id).Balance = newBalance;
+            SaveToDatabase();
+        }
+    }
+}
