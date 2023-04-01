18e7d4a856fbaf090b7a0c8a84bd1f43a0f6f364
Yehor Chervony <chervony.egor@gmail.com>
2023-04-01 19:41:13 +0300

    Try to do task with Interface Segregation and Liskov Substitution principles

---
+++ Lesson4/InterfaceSegregation/InterfaceSegregation/IAdministrator.cs
@@ -0,0 +1,13 @@
+﻿namespace LiskovSubstitution;
+
+public interface IAdministrator : IWorkingWithFile
+{
+    public string ReadFromFile(string filename);
+    public void WriteToFile(string filename);
+    public void DeleteFile(string filename);
+    public void DownloadFile(string filename);
+    public void CopyFile(string filename);
+    public void GetDataFromFile(string filename);
+    public void CheckFile(string filename);
+    public void SaveToFile(string filename);
+}
---
+++ Lesson4/InterfaceSegregation/InterfaceSegregation/WorkingWithFile.cs
@@ -0,0 +1,57 @@
+﻿namespace LiskovSubstitution;
+
+public class WorkingWithFile : IWorkingWithFile
+{
+ public Guid CheckUser(Guid user)
+ {
+  //Checking for user in db
+  return user;
+ }
+
+ public Guid CheckRole(Guid role)
+ {
+  //Checking for user role
+  return role;
+ }
+
+ public string ReadFromFile(string filename)
+ {
+  //Reading from file
+  return ""; //Some text in file
+ }
+
+ public void WriteToFile(string filename)
+ {
+  //Write to file
+ }
+
+ public void DeleteFile(string filename)
+ {
+  //Delete file
+ }
+
+ public void DownloadFile(string filename)
+ {
+  //Download file
+ }
+
+ public void CopyFile(string filename)
+ {
+  //Copy file
+ }
+
+ public void GetDataFromFile(string filename)
+ {
+  //Get data from file
+ }
+
+ public void CheckFile(string filename)
+ {
+  //Check file
+ }
+
+ public void SaveToFile(string filename)
+ {
+  //Save to file
+ }
+}
