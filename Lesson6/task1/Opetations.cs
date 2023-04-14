b0ab77aefc380557976d1fc15e1a3ea373486e95
Yehor Chervony <chervony.egor@gmail.com>
2023-04-15 00:23:20 +0300

    Try to do calc with delegates lambdas

---
+++ Lesson6/task1/Opetations.cs
@@ -0,0 +1,14 @@
+﻿namespace task1;
+
+public class Opetations
+{
+    public delegate float Operation(float a, float b);
+    public static void DoOperation(float a, float b, Operation operation)
+    {
+        Console.WriteLine(operation(a,b));
+    }
+    public static float Add (float a, float b ) => a + b;
+    public static float Subtract (float a, float b ) => a - b;
+    public static float Multiply (float a, float b ) => a * b;
+    public static float Devide (float a, float b ) => a / b;
+}
---
+++ Lesson6/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using task1;
+while (true)
+{
+    Console.Write("First number: ");
+    float a = float.Parse(Console.ReadLine());
+    Console.Write("Choose action (+, -, /, *): ");
+    string userinput = Console.ReadLine();
+    Console.Write("Second number: ");
+    float b = float.Parse(Console.ReadLine());
+    switch (userinput)
+    {
+        case "+":
+            Opetations.DoOperation(a,b,Opetations.Add);
+            break;
+        case "-":
+            Opetations.DoOperation(a,b,Opetations.Subtract);
+            break;
+        case "*":
+            Opetations.DoOperation(a,b,Opetations.Multiply);
+            break;
+        case "/":
+            Opetations.DoOperation(a,b,Opetations.Devide);
+            break;
+    }
+}
