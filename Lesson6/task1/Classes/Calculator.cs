46cbf3df9ddd2bbe1cd342937446be4be6975a18
Yehor Chervony <chervony.egor@gmail.com>
2023-04-22 23:39:33 +0300

    Move calculator to other class

---
+++ Lesson6/task1/Classes/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using task1;
+
+public class Calculator
+{
+    public void CalculatorMain()
+    {
+        Opetations operation = new Opetations();
+        Console.Write("First number: ");
+        float a = float.Parse(Console.ReadLine());
+        Console.Write("Choose action (+, -, /, *): ");
+        string userinput = Console.ReadLine();
+        Console.Write("Second number: ");
+        float b = float.Parse(Console.ReadLine());
+        switch (userinput)
+        {
+            case "+":
+                operation.DoOperation(a,b,operation.Add);
+                break;
+            case "-":
+                operation.DoOperation(a,b,operation.Subtract);
+                break;
+            case "*":
+                operation.DoOperation(a,b,operation.Multiply);
+                break;
+            case "/":
+                operation.DoOperation(a,b,operation.Devide);
+                break;
+        }
+    }
+}
--- Lesson6/task1/Program.cs
+++ Lesson6/task1/Program.cs
@@ -1,27 +1,8 @@
 ﻿using task1;
 
-Opetations operation = new Opetations();
+var calc = new Calculator();
+
 while (true)
 {
-    Console.Write("First number: ");
-    float a = float.Parse(Console.ReadLine());
-    Console.Write("Choose action (+, -, /, *): ");
-    string userinput = Console.ReadLine();
-    Console.Write("Second number: ");
-    float b = float.Parse(Console.ReadLine());
-    switch (userinput)
-    {
-        case "+":
-            operation.DoOperation(a,b,operation.Add);
-            break;
-        case "-":
-            operation.DoOperation(a,b,operation.Subtract);
-            break;
-        case "*":
-            operation.DoOperation(a,b,operation.Multiply);
-            break;
-        case "/":
-            operation.DoOperation(a,b,operation.Devide);
-            break;
-    }
+    calc.CalculatorMain();
 }
